b2036f888b38a2522dbe0e1cd3e3f8e3c760d8ea
bagheera30 <rizkialfian30103@gmail.com>
2023-04-05 00:31:20 +0700

    awal gui

---
+++ src/main/java/com/mycompany/tubespbo/Tubespbo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubespbo;
+
+/**
+ *
+ * @author asus
+ */
+public class Tubespbo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
